dad72773decb3bc0e71c8c356855245152520b48
Joao <joaoviniciusvitral@hotmail.com>
2022-07-08 00:49:21 -0300

    app provider

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
+import { AppRoutes } from '../routes'
+import { AppProvider } from '../context'
 
 function App() {
   return (
-    <></>
+    <AppProvider>
+      <AppRoutes />
+    </AppProvider>
   )
 }
 
