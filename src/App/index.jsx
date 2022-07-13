193bd1e01ef6ddb946e89f5fc98c4b15707aa332
Joao <joaoviniciusvitral@hotmail.com>
2022-07-13 14:28:20 -0300

    login page

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
 import { AppRoutes } from '../routes'
-import { AppProvider } from '../context'
+import { AppProvider, AuthGoogleProvider } from '../context'
 import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
-      <AppProvider>
-        <AppRoutes />
-      </AppProvider>
+      <AuthGoogleProvider>
+        <AppProvider>
+          <AppRoutes />
+        </AppProvider>
+      </AuthGoogleProvider>
     </ChakraProvider>
   )
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Button } from '@chakra-ui/react'
+import { useGoogleAuth } from '../../context';
 
 export function Login() {
-  return <div />;
+  const { signInGoogle } = useGoogleAuth()
+
+  return (
+    <div>
+      <Button onClick={() => signInGoogle()}>
+        Login with Google
+      </Button>
+    </div>
+  );
 }
 
