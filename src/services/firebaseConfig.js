2ac30ea93ca8d0ba706492d2576d159a6cbbffca
Joao <joaoviniciusvitral@hotmail.com>
2022-08-01 16:39:06 -0300

    env update

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXOCQun2h01FP_k_9dq3EsrcSJiEviRFE",
-  authDomain: "priority-todo-def4f.firebaseapp.com",
-  projectId: "priority-todo-def4f",
-  storageBucket: "priority-todo-def4f.appspot.com",
-  messagingSenderId: "195986426623",
-  appId: "1:195986426623:web:d33c4c253c0103a5635aca"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
